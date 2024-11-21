6ade46cfca6cc50b483a3716c7e31c88b232c864
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-11-21 01:19:09 -0300

    chore: referentially grouped prop vars

--- apps/www/src/components/nyx/button/button.vue
+++ apps/www/src/components/nyx/button/button.vue
@@ -9,9 +9,9 @@
 
 <template>
   <InternalPrimitive
-    :as="as"
-    :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)">
+    :as="props.as"
+    :as-child="props.asChild"
+    :class="cn(buttonVariants({ variant: props.variant, size: props.size }), props.class)">
     <slot />
   </InternalPrimitive>
 </template>
