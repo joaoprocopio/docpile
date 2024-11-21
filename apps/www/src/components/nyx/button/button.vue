6567c9b49375934fea4ff4b90484982e1c479dca
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-11-21 01:17:56 -0300

    feat: add stylized button

--- apps/www/src/components/nyx/button/button.vue
+++ apps/www/src/components/nyx/button/button.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+  import type { ButtonProps } from "."
+  import { buttonVariants } from "."
+
+  const props = withDefaults(defineProps<ButtonProps>(), {
+    as: "button",
+  })
+</script>
+
 <template>
-  <button><slot /></button>
+  <InternalPrimitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)">
+    <slot />
+  </InternalPrimitive>
 </template>
