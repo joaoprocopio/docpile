d0beb4ff34ab7a9628950fd328fdee22e83ce377
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-11-21 01:32:43 -0300

    feat: add theme toggle

--- apps/www/nuxt.config.ts
+++ apps/www/nuxt.config.ts
@@ -41,6 +41,7 @@ export default defineNuxtConfig({
   radix: {
     prefix: "Internal",
   },
+
   components: {
     dirs: [
       {
---
+++ apps/www/src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <NyxButton class="rounded-full" variant="secondary" size="icon" @click="isDark = !isDark">
+    <Icon class="size-4" :name="isDark ? 'lucide:moon' : 'lucide:sun'" />
+  </NyxButton>
+</template>
+
+<script setup lang="ts">
+  const colorMode = useColorMode()
+  const isDark = computed({
+    get() {
+      return colorMode.value === "dark"
+    },
+    set() {
+      colorMode.preference = colorMode.value === "dark" ? "light" : "dark"
+    },
+  })
+</script>
