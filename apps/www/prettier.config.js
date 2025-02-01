fd68d0832d283e89c1e1e0d42ab33a2110974568
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-02-01 00:11:49 -0300

    add sort

--- apps/www/prettier.config.js
+++ apps/www/prettier.config.js
@@ -7,6 +7,7 @@ const config = {
   trailingComma: "all",
   quoteProps: "consistent",
   printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
 }
 
 export default config
--- apps/www/src/layouts/default.vue
+++ apps/www/src/layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <header>
+    <header class="flex h-16 items-center px-6">
+      <h1 class="text-3xl font-semibold">foo</h1>
+
       <NyxButton class="rounded-full" variant="secondary" size="icon" @click="isDark = !isDark">
         <Icon class="size-4" :name="isDark ? 'lucide:moon' : 'lucide:sun'" />
       </NyxButton>
