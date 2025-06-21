4022ebd64375ba1cf21e45deff8a72e17a757c67
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-06-21 14:39:12 -0300

    vai

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+async fn hello() -> String {
+    String::from("hello, world!")
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("abcasdfd listenasdfasdfasdfasdfaasdfsdf aaaaaaaaaaaaaaa");
+
+    axum::serve(listener, app).await.unwrap();
 }
