f145e91e22282409d62afb638c178492aecb3503
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-25 00:07:17 -0300

    not found

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,9 @@
 use crate::Server;
-use axum::Router;
+use axum::{Router, http::StatusCode, response::IntoResponse};
 use tower_http::trace::TraceLayer;
 
 pub fn routes() -> Router<Server> {
-    Router::new().layer(TraceLayer::new_for_http())
+    Router::new()
+        .layer(TraceLayer::new_for_http())
+        .fallback(async || StatusCode::NOT_FOUND.into_response())
 }
