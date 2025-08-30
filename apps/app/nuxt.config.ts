26d529d3957ca78f85e6a0e042e94137371b7d3a
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-30 09:45:42 -0300

    monorepo

---
+++ apps/app/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/scripts"
+  ]
+})
