eed1efab7f4520ad7a9e7ad5a07120150317f103
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-30 10:07:26 -0300

    vai

--- apps/app/eslint.config.mjs
+++ apps/app/eslint.config.mjs
@@ -1,6 +1,34 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import * as compat from "@eslint/compat";
+import prettier from "eslint-plugin-prettier/recommended";
+import sort from "eslint-plugin-simple-import-sort";
+import path from "path";
+import url from "url";
 
-export default withNuxt(
-  // Your custom configs here
-)
+import withNuxt, { defineFlatConfigs } from "./.nuxt/eslint.config.mjs";
+
+function withGitignore(config) {
+  const filename = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+  const gitignore = path.resolve(dirname, ".gitignore");
+
+  return defineFlatConfigs(config, compat.includeIgnoreFile(gitignore));
+}
+
+function withSimpleImportSort(config) {
+  return defineFlatConfigs(config, {
+    plugins: {
+      "simple-import-sort": sort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  });
+}
+
+function withPrettier(config) {
+  return defineFlatConfigs(config, prettier);
+}
+
+export default withSimpleImportSort(withGitignore(withPrettier(withNuxt())));
--- apps/app/nuxt.config.ts
+++ apps/app/nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  compatibilityDate: "latest",
   modules: [
     "@nuxt/eslint",
     "@nuxt/fonts",
     "@nuxt/icon",
-    "@nuxt/image",
-    "@nuxt/scripts"
-  ]
+    "@nuxt/image"
+  ],
+  srcDir: "src/",
+  ssr: false,
+  telemetry: false,
+  pages: false,
+  imports: { autoImport: false },
+  devtools: { enabled: true },
 })
