f1b898dd208c47623ab55384e6c8dc6b3b7e8fbf
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-31 00:30:14 -0300

    tailwind

--- apps/app/nuxt.config.ts
+++ apps/app/nuxt.config.ts
@@ -1,10 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   extends: ["./src/editor"],
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
   ssr: true,
   imports: { autoImport: false },
   devtools: { enabled: true },
+  css: ["~/shared/assets/tailwind.css"],
   srcDir: "src/",
   compatibilityDate: "latest",
+  vite: {
+    plugins: [tailwindcss()],
+  },
   telemetry: false,
 });
