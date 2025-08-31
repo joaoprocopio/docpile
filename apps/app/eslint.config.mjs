bf9c602d6ccc591a81a5aba8a7bec34c4cf8d745
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-30 23:51:15 -0300

    prettier

--- apps/app/eslint.config.mjs
+++ apps/app/eslint.config.mjs
@@ -1,35 +1,38 @@
-import * as compat from '@eslint/compat'
-import sort_ from 'eslint-plugin-simple-import-sort'
-import path from 'path'
-import url from 'url'
+import * as compat from "@eslint/compat";
+import prettier_ from "eslint-plugin-prettier/recommended";
+import sort_ from "eslint-plugin-simple-import-sort";
+import path from "path";
+import url from "url";
 
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+/** @returns {import("eslint").Linter.Config} */
+function prettier() {
+  return prettier_;
+}
 
 /** @returns {import("eslint").Linter.Config} */
 function gitignore() {
-  const filename = url.fileURLToPath(import.meta.url)
-  const dirname = path.dirname(filename)
-  const gitignore = path.resolve(dirname, '.gitignore')
+  const filename = url.fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+  const gitignore = path.resolve(dirname, ".gitignore");
 
-  return compat.includeIgnoreFile(gitignore)
+  return compat.includeIgnoreFile(gitignore);
 }
 
 /** @returns {import("eslint").Linter.Config} */
 function sort() {
   return {
     plugins: {
-      'simple-import-sort': sort_,
+      "simple-import-sort": sort_,
     },
     rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
-  }
+  };
 }
 
-const config = withNuxt(
-  gitignore(),
-  sort(),
-)
+const config = withNuxt(gitignore(), prettier(), sort());
 
-export default config
+export default config;
--- apps/app/nuxt.config.ts
+++ apps/app/nuxt.config.ts
@@ -1,15 +1,10 @@
 export default defineNuxtConfig({
-  extends: ['./src/auth'],
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon', '@nuxt/image'],
+  extends: ["./src/auth"],
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image"],
   ssr: true,
   imports: { autoImport: false },
   devtools: { enabled: true },
-  srcDir: 'src/',
-  compatibilityDate: 'latest',
+  srcDir: "src/",
+  compatibilityDate: "latest",
   telemetry: false,
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-  },
-})
+});
