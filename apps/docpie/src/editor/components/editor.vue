b89596f82925b129b15bc0a5a56838219bec98f8
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-09-11 20:32:29 -0300

    include

--- apps/docpie/src/editor/components/editor.vue
+++ apps/docpie/src/editor/components/editor.vue
@@ -1,8 +1,5 @@
-<template>
-  <div ref="editor" />
-</template>
-
 <script setup lang="ts">
+import { baseKeymap } from "codex/commands";
 import { history, redo, undo } from "codex/history";
 import { keymap } from "codex/keymap";
 import { schema } from "codex/schema-basic";
@@ -10,7 +7,7 @@ import { EditorState } from "codex/state";
 import { EditorView } from "codex/view";
 import { onMounted, useTemplateRef } from "vue";
 
-const ref = useTemplateRef("editor");
+const editorRef = useTemplateRef("editor");
 
 onMounted(() => {
   const state = EditorState.create({
@@ -21,9 +18,11 @@ onMounted(() => {
         "Mod-z": undo,
         "Mod-y": redo,
       }),
+      keymap(baseKeymap),
     ],
   });
-  const view = new EditorView(ref.value, {
+
+  const view = new EditorView(editorRef.value, {
     state: state,
     dispatchTransaction(tr) {
       const newState = view.state.apply(tr);
@@ -32,5 +31,9 @@ onMounted(() => {
   });
 });
 </script>
+
+<template>
+  <div ref="editor" />
+</template>
 
 <style src="codex/view/style/prosemirror.css" />
