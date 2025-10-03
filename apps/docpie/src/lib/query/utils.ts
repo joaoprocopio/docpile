b36b66aabbbad919f80bc0592ed0316a4ee62aca
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-10-03 02:51:43 +0100

    vai

--- apps/docpie/src/lib/query/utils.ts
+++ apps/docpie/src/lib/query/utils.ts
@@ -1,4 +1,39 @@
-import type { DefaultError, UseMutationOptions } from "@tanstack/vue-query"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { DefaultError, QueryKey, UseMutationOptions } from "@tanstack/vue-query"
+import { queryOptions } from "@tanstack/vue-query"
+
+export const RootKey = Symbol()
+export type TKey = string
+
+export type TQueries<TRootKey extends TKey, TKeys extends TKey> = {
+    [RootKey]: [TRootKey]
+    all(): [TRootKey]
+} & {
+    [K in TKeys]: (...args: any[]) => {
+        queryKey: [TRootKey, ...QueryKey]
+    }
+}
+export function defineQueries<TRootKey extends TKey, TKeys extends TKey>(
+    rootKey: TRootKey,
+    keyring: Record<TKeys, any>,
+): TQueries<TRootKey, TKeys> {
+    return {
+        [RootKey]: [rootKey],
+        all() {
+            return [rootKey]
+        },
+        ...keyring,
+    }
+}
+defineQueries("users", {
+    detail: () =>
+        queryOptions({
+            queryKey: ["users", "detail"],
+            queryFn: () => {
+                /*  */
+            },
+        }),
+})
 
 export function mutationOptions<
     TData = unknown,
