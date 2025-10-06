1a13391d5244a562a5e71f269fcea21e84516304
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-10-06 01:05:16 +0100

    vai

--- apps/docpie/src/lib/query/utils.ts
+++ apps/docpie/src/lib/query/utils.ts
@@ -1,20 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { DefaultError, MutationKey, UseMutationOptions } from "@tanstack/vue-query"
+import type { DefaultError, MutationKey, QueryKey, UseMutationOptions } from "@tanstack/vue-query"
 
 export function defineQueries<
     const TRootKey extends string,
-    const TDefs extends {
+    const TDefs extends Record<string, (...args: any[]) => any>,
+>(
+    _: TRootKey,
+    defs: {
         [TDef in keyof TDefs]: TDef extends "all"
             ? () => [TRootKey] | readonly [TRootKey]
             : TDefs[TDef] extends (...args: infer TArgs) => infer TReturn
-              ? TReturn extends { queryKey: infer QK }
-                  ? QK extends any[]
-                      ? (...args: TArgs) => TReturn & { queryKey: [TRootKey, ...QK] }
-                      : TDefs[TDef]
+              ? TReturn extends { queryKey: [...infer _] | readonly [...infer _] }
+                  ? (...args: TArgs) => Omit<TReturn, "queryKey"> & {
+                        queryKey: readonly [TRootKey, ...QueryKey]
+                    }
                   : TDefs[TDef]
               : TDefs[TDef]
     },
->(__rootKey: TRootKey, defs: TDefs) {
+) {
     return defs
 }
 
