df5b64bbb21c28e4c8f2d86a7740030dc26ebfb6
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-10-13 17:34:12 +0100

    vai

--- crates/docpie/src/db/migrations/20251007003322_orgs.down.sql
+++ crates/docpie/src/db/migrations/20251007003322_orgs.down.sql
@@ -1,4 +1,9 @@
 -- Add down migration script here
+DROP INDEX idx_org_membership_user_id;
+DROP INDEX idx_org_membership_org_id;
+DROP INDEX idx_org_membership_prevent_duplicate;
 DROP TABLE org_membership;
+DROP TYPE org_membership_status;
+DROP TYPE org_membership_role;
 DROP TABLE orgs;
 DROP TYPE org_status;
