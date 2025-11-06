737bc1b1cd6d04658bb94f4668f78fb5f9df3a0b
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-06 19:07:06 +0000

    vai

--- apps/docpie/src/router.options.ts
+++ apps/docpie/src/router.options.ts
@@ -10,50 +10,60 @@ const routes = <Readonly<RouteRecordRaw[]>>[
         component: () => import("~/pages/home"),
     },
     {
-        path: "/onboarding",
-        component: () => import("~/pages/onboarding"),
-        meta: {
-            layout: false,
-            middleware: "onboarding",
-        },
+        path: "/:slug",
         children: [
             {
-                name: OnboardingRoutes.Intro,
-                path: "",
-                component: () => import("~/pages/onboarding-intro"),
-            },
-            {
-                name: OnboardingRoutes.Theme,
-                path: "theme",
-                component: () => import("~/pages/onboarding-theme"),
-            },
-            {
-                name: OnboardingRoutes.Org,
-                path: "org",
-                component: () => import("~/pages/onboarding-org"),
-            },
-            {
-                name: OnboardingRoutes.Team,
-                path: "team",
-                component: () => import("~/pages/onboarding-team"),
+                path: "onboarding",
+                component: () => import("~/pages/onboarding"),
+                meta: {
+                    layout: false,
+                    middleware: "onboarding",
+                },
+                children: [
+                    {
+                        name: OnboardingRoutes.Intro,
+                        path: "",
+                        component: () => import("~/pages/onboarding-intro"),
+                    },
+                    {
+                        name: OnboardingRoutes.Theme,
+                        path: "theme",
+                        component: () => import("~/pages/onboarding-theme"),
+                    },
+                    {
+                        name: OnboardingRoutes.Org,
+                        path: "org",
+                        component: () => import("~/pages/onboarding-org"),
+                    },
+                    {
+                        name: OnboardingRoutes.Team,
+                        path: "team",
+                        component: () => import("~/pages/onboarding-team"),
+                    },
+                ],
             },
         ],
     },
     {
-        name: AuthRoutes.SignIn,
-        path: "/signin",
-        component: () => import("~/pages/signin"),
-        meta: {
-            layout: "auth",
-        },
-    },
-    {
-        name: AuthRoutes.SignUp,
-        path: "/signup",
-        component: () => import("~/pages/signup"),
-        meta: {
-            layout: "auth",
-        },
+        path: "/auth",
+        children: [
+            {
+                name: AuthRoutes.SignIn,
+                path: "signin",
+                component: () => import("~/pages/signin"),
+                meta: {
+                    layout: "auth",
+                },
+            },
+            {
+                name: AuthRoutes.SignUp,
+                path: "signup",
+                component: () => import("~/pages/signup"),
+                meta: {
+                    layout: "auth",
+                },
+            },
+        ],
     },
 ]
 
