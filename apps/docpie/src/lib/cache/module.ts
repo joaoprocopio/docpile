643e66dd3359a2756a0eff6db355454dd4675449
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-25 00:28:38 +0000

    runtime

--- apps/docpie/src/lib/cache/module.ts
+++ apps/docpie/src/lib/cache/module.ts
@@ -3,5 +3,5 @@ import { addPlugin, createResolver, defineNuxtModule } from "@nuxt/kit"
 export default defineNuxtModule(() => {
     const resolver = createResolver(import.meta.url)
 
-    addPlugin(resolver.resolve("./plugins/client.ts"))
+    addPlugin(resolver.resolve("./runtime/plugins/client.ts"))
 })
