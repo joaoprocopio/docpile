4ec947882c85a9c631b1287770c8ef7fb3e02ada
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-25 01:57:39 +0000

    vai

--- apps/docpie/src/lib/cache/utils.ts
+++ apps/docpie/src/lib/cache/utils.ts
@@ -4,44 +4,44 @@ import type { DefaultError, MutationKey, UseMutationOptions } from "~/lib/cache"
 export type AnyFn = (...args: any[]) => any
 export type Definition = Record<string, AnyFn>
 
-export type DefineKeys<TKey, TDefine> = {
-    [TDef in keyof TDefine]: TDefine[TDef] extends (...args: infer TArgs) => infer TReturn
+export type KeysDefinition<TNamespace, TDefinition> = {
+    [TDef in keyof TDefinition]: TDefinition[TDef] extends (...args: infer TArgs) => infer TReturn
         ? TReturn extends [...infer QK]
-            ? (...args: TArgs) => readonly [TKey, ...QK]
+            ? (...args: TArgs) => readonly [TNamespace, ...QK]
             : never
         : never
 }
 
-export type DefineKeyring<
-    TKey extends string,
-    TDefineQueryKeys extends Definition,
-    TDefineMutationKeys extends Definition,
-    // TDefineQueries extends Define,
-    // TDefineMutations extends Define,
+export type KeyringDefinition<
+    TNamespace extends string,
+    TQueryKeysDefinition extends Definition,
+    TMutationKeysDefinition extends Definition,
+    // TQueriesDefinition extends Definition,
+    // TMutationsDefinition extends Definition,
 > = {
     keys: {
-        queries: DefineKeys<TKey, TDefineQueryKeys>
-        mutations: DefineKeys<TKey, TDefineMutationKeys>
+        queries: KeysDefinition<TNamespace, TQueryKeysDefinition>
+        mutations: KeysDefinition<TNamespace, TMutationKeysDefinition>
     }
     // queries: null
     // mutations: null
 }
 
-export function defineKeyring<const TKey extends string>(_: TKey) {
+export function defineKeyring<const TNamespace extends string>(_: TNamespace) {
     return function <
-        const TDefineQueryKeys extends Definition,
-        const TDefineMutationKeys extends Definition,
-        const TKeyring extends DefineKeyring<
-            TKey,
-            TDefineQueryKeys,
-            TDefineMutationKeys
-        > = DefineKeyring<TKey, TDefineQueryKeys, TDefineMutationKeys>,
-    >(keyring: TKeyring) {
+        const TQueryKeysDefinition extends Definition,
+        const TMutationKeysDefinition extends Definition,
+        const TKeyringDefinition extends KeyringDefinition<
+            TNamespace,
+            TQueryKeysDefinition,
+            TMutationKeysDefinition
+        > = KeyringDefinition<TNamespace, TQueryKeysDefinition, TMutationKeysDefinition>,
+    >(keyring: TKeyringDefinition) {
         return keyring
     }
 }
 
-export function key<const TKey extends readonly unknown[]>(...args: TKey) {
+export function key<const TKeys extends readonly unknown[]>(...args: TKeys) {
     return args
 }
 
