77784dc7344fedcd978fa40e1463ac10b18f9191
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-26 07:07:23 +0000

    vai

--- apps/docpie/src/middleware/invite.ts
+++ apps/docpie/src/middleware/invite.ts
@@ -2,7 +2,9 @@ import { defineNuxtRouteMiddleware } from "#app"
 
 // TODO: otimizar o middleware pra exibir um estado de loading melhor
 // TODO: exibir um belo estado de erro para os diferentes casos
-export default defineNuxtRouteMiddleware((to) => {
-    console.log("invite", to)
+export default defineNuxtRouteMiddleware(() => {
+    // aqui a gente tem o token. eh só bater no backend, pegar o nome do cara, o nome da org, redireciona pra tela de login.
+    // altera o fluxo um pouco, ele vai fazer o cadastro normal, vai pular a etapa de criar a org e ir direto pro onboarding.
+
     return undefined
 })
