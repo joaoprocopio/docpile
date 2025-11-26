4ce6b6b8f58fd2b22b85759c2296aa5d309c53da
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-26 20:16:57 +0000

    vai

--- apps/docpie/src/pages/signin/signin.vue
+++ apps/docpie/src/pages/signin/signin.vue
@@ -131,7 +131,7 @@ const [showPassword, toggleShowPassword] = useToggle(false)
             <Button
                 :disabled="isLoading"
                 type="submit"
-                variant="secondary">
+                variant="default">
                 <Spinner v-if="isLoading" />
                 <template v-else> Continue with email </template>
             </Button>
