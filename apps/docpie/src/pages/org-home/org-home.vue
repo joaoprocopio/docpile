31ddaffe5e5a6a0a999dfd644b0b685cefa1af9b
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-28 02:35:53 +0000

    vai

--- apps/docpie/src/pages/org-home/org-home.vue
+++ apps/docpie/src/pages/org-home/org-home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { Editor } from "~/lib/prosemirror/components/editor"
+import { SidebarTrigger } from "~/lib/ui/sidebar"
 </script>
 
 <template>
     <div>
+        <SidebarTrigger />
         <h1 class="font-bold">editor</h1>
         <Editor />
     </div>
