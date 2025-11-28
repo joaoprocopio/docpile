ae6ed57e558ded580e5bea1dd11b9cb1a154113c
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-11-28 02:09:19 +0000

    wrapper

--- apps/docpie/src/lib/ui/sidebar/index.ts
+++ apps/docpie/src/lib/ui/sidebar/index.ts
@@ -32,6 +32,7 @@ export { default as SidebarProvider } from "./SidebarProvider.vue"
 export { default as SidebarRail } from "./SidebarRail.vue"
 export { default as SidebarSeparator } from "./SidebarSeparator.vue"
 export { default as SidebarTrigger } from "./SidebarTrigger.vue"
+export { default as SidebarWrapper } from "./SidebarWrapper.vue"
 export { useSidebar } from "./utils"
 
 export const sidebarMenuButtonVariants = cva(
